ded8fb12949462380298d2fec41f8679b1b2ffab
Cleyton Melo <cleytonw11@gmail.com>
2025-09-17 17:00:57 -0300

    Abstração, encapsulamento, herança, polimorfismo, classes abstratas e interfaces

---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exemplo_poo.Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            saldo = saldoInicial;
+        }
+        public int NumeroConta { get; set; }
+        private decimal saldo;
+
+        public void Sacar(decimal valor)
+        {
+            if (saldo <= valor)
+            {
+                Console.WriteLine("Saldo insuficiente para saque.");
+            }
+            else
+            {
+                Console.WriteLine($"Saque de {valor} realizado com sucesso.");
+                saldo -= valor;
+            }
+        }
+
+        public void exibirSaldo()
+        {
+            Console.WriteLine($"Saldo atual: {saldo}");
+        }
+    }
+}
